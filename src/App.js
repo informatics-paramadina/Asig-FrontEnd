3ec165e308b86fa7c59f03c0d780a3de3788c73b
ghifari-ti <muhammad.ghifari@students.paramadina.ac.id>
2021-09-09 16:14:18 +0700

    add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React, {useEffect} from 'react';
-import {useDispatch} from 'react-redux';
-import 'assets/css/style.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HomePages from 'pages/Home';
-import GuestRoutes from 'Components/Routes/GuestRoutes';
-import Login from 'pages/login';
-import MyDasboard from 'pages/Dasboard';
-import MemberRoutes from 'Components/Routes/MemberRoutes';
-import Register from 'pages/register';
-import Unauthenticated from 'pages/401';
-import {setAuthorizationHeader} from 'config/axios';
-import users from 'Constant/Api/users';
-import {populateProfile} from 'Store/action/users';
-import Shcedule from 'parts/partsDasboard/Shcedule';
-import Sertifikat from 'parts/partsDasboard/Sertifikat';
-import RGame from 'pages/RGame';
-import RMiniGame from 'pages/RMiniGame';
-import RTalkShow from 'pages/RTalkShow';
-import Anim from 'pages/Anim';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "assets/css/style.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePages from "pages/Home";
+import GuestRoutes from "Components/Routes/GuestRoutes";
+import Login from "pages/login";
+import MyDasboard from "pages/Dasboard";
+import MemberRoutes from "Components/Routes/MemberRoutes";
+import Register from "pages/register";
+import Unauthenticated from "pages/401";
+import { setAuthorizationHeader } from "config/axios";
+import users from "Constant/Api/users";
+import { populateProfile } from "Store/action/users";
+import Shcedule from "parts/partsDasboard/Shcedule";
+import Sertifikat from "parts/partsDasboard/Sertifikat";
+import RGame from "pages/RGame";
+import RMiniGame from "pages/RMiniGame";
+import RTalkShow from "pages/RTalkShow";
+import Anim from "pages/Anim";
 
 function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const loader = document.querySelector(".loader");
+
+  useEffect(() => {
+    loader.classList.add("loader--hide");
+  }, []);
 
   useEffect(() => {
     let session = null;
@@ -29,14 +34,14 @@ function App() {
       setAuthorizationHeader(session.token);
 
       users.details().then((details) => {
-        dispatch(populateProfile(details))
-      })
+        dispatch(populateProfile(details));
+      });
     }
-  }, [dispatch])
+  }, [dispatch]);
 
   return (
     <>
-      <Router> 
+      <Router>
         <Switch>
           <GuestRoutes path="/login" component={Login} />
           <GuestRoutes path="/register" component={Register} />
