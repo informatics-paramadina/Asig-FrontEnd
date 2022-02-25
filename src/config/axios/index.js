aa9f8491f571cc2e2416cf5a4c38f79be2d79020
muhammad al ghifari <muhamadalghifari97@gmail.com>
2022-02-25 16:52:48 +0700

    update endpoint

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import errorHandler from './errorHandler';
 
 const instance = axios.create({
-    baseURL: 'https://api.himti.my.id',
+    baseURL: 'https://api.himti.id',
 })
 instance.interceptors.response.use((response) => response.data, errorHandler)
 
